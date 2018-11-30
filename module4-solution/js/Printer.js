f8eb1093a7729150d7f497ed50de62ca491e673d
ealfzam <alfredo.zamora@ericsson.com>
2018-11-30 10:17:28 +0100

    implemented module4-solution

---
+++ module4-solution/js/Printer.js
@@ -0,0 +1,28 @@
+(function(window) {
+
+  var printerNames = {};
+  printerNames.property = "names";
+  printerNames.print = function(jsonfile) {
+  	
+  	if(jsonfile.hasOwnProperty(this.property)) {
+  		names = jsonfile["names"];
+	  
+	  	for (var i = 0; i < names.length; i++) {
+
+	  	  var firstLetter = names[i].toLowerCase().charAt(0);
+
+	  	  if ('j' === firstLetter) {
+	    	byeSpeaker.speak(names[i]);
+	  	  } else {
+	    	helloSpeaker.speak(names[i]);
+	  	  }
+		}
+  	} else {
+  		console.log("json file does not contains 'names' property");
+  	}
+  }
+
+  window.printerNames = printerNames;
+
+})(window);
+
---
+++ module4-solution/js/script.js
@@ -0,0 +1,59 @@
+// *******************************
+// START HERE IF YOU WANT A MORE CHALLENGING STARTING POINT FOR THIS ASSIGNMENT
+// *******************************
+//
+// Module 4 Assignment Instructions.
+//
+// The idea of this assignment is to take an existing array of names
+// and then output either Hello 'Name' or Good Bye 'Name' to the console.
+// The program should say "Hello" to any name except names that start with a "J"
+// or "j", otherwise, the program should say "Good Bye". So, the final output
+// on the console should look like this:
+/*
+Hello Yaakov
+Good Bye John
+Good Bye Jen
+Good Bye Jason
+Hello Paul
+Hello Frank
+Hello Larry
+Hello Paula
+Hello Laura
+Good Bye Jim
+
+WARNING!!! WARNING!!!
+The code does NOT currently work! It is YOUR job to make it work
+as described in the requirements and the steps in order to complete this
+assignment.
+WARNING!!! WARNING!!!
+
+*/
+
+// STEP 1:
+// Wrap the entire contents of script.js inside of an IIFE
+// See Lecture 52, part 2
+// (Note, Step 2 will be done in the SpeakHello.js file.)
+
+(function() {
+
+  // the array of names are taken from a json file located in ../resources/ folder
+  loadJSON('../resources/names.json');
+
+  // steps related FOR and IF statements can be found in Printer.js file
+
+})();
+
+// asyncronous method to retrieve and parse a json file
+function loadJSON(f) {   
+
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', f, true);
+    xobj.onreadystatechange = function () {
+      if (xobj.readyState === 4 && xobj.status === 200) {
+        var retval = JSON.parse(xobj.responseText);
+        printerNames.print(retval);
+      }
+    }
+    xobj.send();
+}
